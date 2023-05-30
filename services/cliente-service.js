8f9c338e176a8a974eeba68b193a86011ad07fc0
Wagner Oliveira <contato-wagneros@outlook.com>
2023-05-30 14:30:01 -0300

    Commit Final

--- services/cliente-service.js
+++ services/cliente-service.js
@@ -1,7 +1,10 @@
 const listaClientes = () =>{
     return fetch(`http://localhost:3000/profile`)
     .then( resposta => {
-        return resposta.json()
+        if(resposta.ok){
+            return resposta.json()
+        }
+        throw new Error('Não foi possível listar os clientes')
     })
 }
 
@@ -17,19 +20,58 @@ const criaCliente = (nome, email) => {
         })
     })
     .then( resposta => {
-        return resposta.body
+        if(resposta.ok){
+            return resposta.body
+        }
+        throw new Error('Não foi possível criar um cliente')
     })
 }
 
 const removeCliente = (id) =>{
     return fetch (`http://localhost:3000/profile/${id}`,{
         method: 'DELETE'
+    }).then(resposta => {
+        if(!resposta.ok){
+            throw new Error('Não foi possível remover um cliente')
+        }
     })
 }
 
+const detalhaCliente = (id) => {
+    return fetch (`http://localhost:3000/profile/${id}`
+        .then(resposta =>{
+            if(resposta.ok){
+                return resposta.json()
+            }
+            throw new Error('Não foi possível detalhar o cliente')
+        })
+    )
+
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`,{
+        method: 'PUT',
+        headers: {
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.json()
+        }
+        throw new Error('Não foi possível atualizar o cliente')
+    })
+}
 
 export const clienteService = {
     listaClientes,
     criaCliente,
-    removeCliente
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 }
